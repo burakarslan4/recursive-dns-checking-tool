96f9a0b9a5299101afaaf2cae2f4b7322cf697a3
Burak <117441716+burakarslan4@users.noreply.github.com>
2023-10-31 19:39:52 +0300

    Update recursive_dns_ip_scanner.py

--- recursive_dns_ip_scanner.py
+++ recursive_dns_ip_scanner.py
@@ -3,7 +3,6 @@ import subprocess
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-import ssl
 import os  # Import the os module
 
 # Add your email account information here
@@ -13,8 +12,7 @@ email_to = 'mail@domain.com'
 
 # Add email server and port information here
 email_server = 'sandbox.smtp.mailtrap.io'
-email_port = 465  # You can set it to 587 or 465 for SSL
-use_ssl = True  # Should SSL be used (True/False)
+email_port = 25  # 25,587
 
 # Add send_email_while_empty variable (set to True to always send email)
 send_email_while_empty = True
@@ -28,10 +26,6 @@ body = 'The following IP addresses are found:\n\n'
 
 # Create a list to store valid IP addresses
 valid_ips = []
-
-context = None
-if use_ssl:
-    context = ssl.create_default_context()
 
 for subnet in subnets:
     network = ipaddress.IPv4Network(subnet, strict=False)
@@ -51,7 +45,7 @@ for subnet in subnets:
         except subprocess.CalledProcessError:
             pass
 
-# Check the send_email_while_empty variable
+# Check if there are valid IPs
 if send_email_while_empty or valid_ips:
     # Create an email message
     msg = MIMEMultipart()
@@ -65,13 +59,9 @@ if send_email_while_empty or valid_ips:
     with open('valid_ips.txt', 'w') as file:
         file.write(ip_list_text)
 
-    # Connect to the email server and send the email (with or without SSL)
+    # Connect to the email server and send the email (without SSL)
     try:
-        if use_ssl:
-            server = smtplib.SMTP_SSL(email_server, email_port, context=context)
-        else:
-            server = smtplib.SMTP(email_server, email_port)
-
+        server = smtplib.SMTP(email_server, email_port)
         server.login(email_user, email_password)
         text = msg.as_string()
         server.sendmail(email_user, email_to, text)
@@ -80,7 +70,23 @@ if send_email_while_empty or valid_ips:
     except Exception as e:
         print(f'An error occurred while sending the email: {str(e)}')
 else:
-    print('Query results are empty, email not sent.')
+    # If no valid IPs were found, send an email with a specific message
+    msg = MIMEMultipart()
+    msg['From'] = email_user
+    msg['To'] = email_to
+    msg['Subject'] = subject
+    msg.attach(MIMEText("Good news! No IP addresses responding to recursive DNS queries were found.", 'plain'))
+
+    # Connect to the email server and send the email (without SSL)
+    try:
+        server = smtplib.SMTP(email_server, email_port)
+        server.login(email_user, email_password)
+        text = msg.as_string()
+        server.sendmail(email_user, email_to, text)
+        server.quit()
+        print('Email sent!')
+    except Exception as e:
+        print(f'An error occurred while sending the email: {str(e)}')
 
 # After the task is complete, delete the valid_ips.txt file
 if os.path.exists('valid_ips.txt'):
